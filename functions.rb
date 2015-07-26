57649bc06ceadd83356ffdf32b967a16fac49399
Niklas Sandstrom <mr.n.sandstrom@gmail.com>
2015-07-27 00:02:26 +0200

    adding pi_piper

---
+++ functions.rb
@@ -0,0 +1,47 @@
+require "net/http"
+
+
+def say (text)
+	pid = fork do
+		blink
+	end
+	`echo #{text} | #{Fembot}`
+	if Target == :chromebook
+		brightness 900
+	end
+	Process.kill("KILL", pid)
+end
+
+def dumbMode
+	while true
+		say (rand*1000).to_i.to_s
+		sleep 1.5
+	end
+end
+
+def blink
+	while true
+		sleep Blink_period/2
+		if Target == :chromebook
+			brightness 500
+		end
+		sleep Blink_period/2
+		if Target == :chromebook
+			brightness 900
+		end
+	end
+end
+
+def brightness(brightness)
+	cmd = "echo \"#{brightness}\">/sys/class/backlight/intel_backlight/brightness"
+	system( cmd )
+end
+
+def tryServer(request)
+	begin
+		x = Net::HTTP.get(URI.parse("http://192.168.1.23:34444/#{request}"))
+		x
+	rescue
+		"none"
+	end
+end
--- new.rb
+++ new.rb
@@ -1,56 +1,19 @@
-require "net/http"
-require 'pi_piper'
 require_relative 'constants.rb'
+require_relative 'functions.rb'
 #load 'fembot.txt'
 
 
 
-def say (text)
-	pid = fork do
-		blink
-	end
-	`echo #{text} | #{Fembot}`
-	if Target == "chromebook"
-		brightness 900
-	end
-	Process.kill("KILL", pid)
+Target = :chromebook
+
+if Target != :chromebook
+	puts "will require Pi Piper"
+	require 'pi_piper'
+else
+	puts "will NOT require Pi Piper"
+
 end
 
-def dumbMode
-	while true
-		say (rand*1000).to_i.to_s
-		sleep 1.5
-	end
-end
-
-def blink
-	while true
-		sleep Blink_period/2
-		if Target == "chromebook"
-			brightness 500
-		end
-		sleep Blink_period/2
-		if Target == "chromebook"
-			brightness 900
-		end
-	end
-end
-
-def brightness(brightness)
-	cmd = "echo \"#{brightness}\">/sys/class/backlight/intel_backlight/brightness"
-	system( cmd )
-end
-
-def tryServer(request)
-	begin
-		x = Net::HTTP.get(URI.parse("http://192.168.1.23:34444/#{request}"))
-		x
-	rescue
-		"none"
-	end
-end
-
-Target = "chromebook"
 
 if ARGV.include? "-t" then
     text = ARGV[ARGV.index("-t")+1..ARGV.size].join(" ")
